bee1a4a9effc6e408d25ea23cf848397d13152a6
Michael Harrison <dakpluto@gmail.com>
2022-06-04 06:57:38 -0500

    Lesson 48 complete

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Component } from 'react';
 import './App.css';
 
 import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor() {
@@ -43,21 +44,15 @@ class App extends Component {
 
     return (
       <div className='App'>
-        <input 
-          className='search-box' 
-          type='search' 
-          placeholder='search monsters' 
-          onChange={onSearchChange} 
-        />
         
-        {/* {filteredMonsters.map((monster) => {
-          return (
-            <div key={monster.id}>
-              <h1>{monster.name}</h1>
-            </div>
-          );
-        })} */}
-        <CardList monsters={filteredMonsters} />
+        <SearchBox 
+          onChangeHandler={onSearchChange} 
+          placeholder='Search Monsters' 
+          className='monsters-search-box' 
+        />
+        <CardList 
+          monsters={filteredMonsters} 
+        />
       </div>
     );
   }
